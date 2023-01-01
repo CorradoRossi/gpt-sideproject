18a93dbfc3d9601f0074881162c798b84d4f5ce6
Rossi Meacham <rossimeacham@gmail.com>
2023-01-01 16:22:08 -0500

    firebase implementation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export default function Home() {
   const [timeInput, setTimeInput] = useState("");
   const [seasonInput, setSeasonInput] = useState("");
   const [stateInput, setStateInput] = useState("");
   const [result, setResult] = useState("");
+
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log(analytics);
 
   async function onSubmit(event) {
     event.preventDefault();
